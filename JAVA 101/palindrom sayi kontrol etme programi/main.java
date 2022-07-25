187defc947b60d56dd185796d6e5fc863e8a0218
Ahmet Sabri Bostan <107485174+bostansabri@users.noreply.github.com>
2022-07-25 23:16:42 +0300

    Add files via upload

---
+++ JAVA 101/palindrom sayi kontrol etme programi/main.java
@@ -0,0 +1,34 @@
+//www.patika.dev
+
+import java.util.Scanner;
+
+public class main {
+
+    static boolean isPalidrom(int number){
+
+        int temp=number, reverseNumber=0, lastNumber;
+
+        while (temp!=0){
+            lastNumber=temp%10;
+            reverseNumber=(reverseNumber*10)+lastNumber;
+            temp/=10;
+        }
+        if (number==reverseNumber){
+            System.out.println("Sayi Palindrom Bir Sayidir!");
+            return true;
+        }else {
+            System.out.println("Sayi Palindrom Bir Sayi Degildir!");
+           return false;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+
+        int number;
+        System.out.print("Bir Sayi Giriniz: ");
+        number=scn.nextInt();
+
+        System.out.println(isPalidrom(number));
+
+    }
+}
