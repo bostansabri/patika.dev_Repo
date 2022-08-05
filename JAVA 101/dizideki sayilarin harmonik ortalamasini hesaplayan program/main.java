2cec557e35a3ff127f2670b843a0f871823b9cb7
Ahmet Sabri Bostan <107485174+bostansabri@users.noreply.github.com>
2022-08-05 21:11:46 +0300

    Add files via upload

---
+++ JAVA 101/dizideki sayilarin harmonik ortalamasini hesaplayan program/main.java
@@ -0,0 +1,19 @@
+public class main {
+    public static void main(String[] args) {
+
+        double [] arr={1,2,3,4,5};
+        double sum=0.0;
+        int mult=1;
+
+        for (int i=0; i<arr.length; i++){
+            sum+=arr[i];
+        }
+        for (int i=0; i<arr.length; i++){
+            mult*=arr[i];
+        }
+
+        double harAvarage= (arr.length*mult)/sum;
+        System.out.println(harAvarage);
+
+    }
+}
